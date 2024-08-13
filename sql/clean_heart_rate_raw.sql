bb122cd0d96b620fefc64142c97e26dcf35e72a4
Summer Sproull <summer.sproull@gmail.com>
2024-08-12 23:06:48 -0400

    added comment to parced timestamp function

--- sql/clean_heart_rate_raw.sql
+++ sql/clean_heart_rate_raw.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `crack-case-431914-j8.Capstone.heart_rate` AS
 SELECT 
     Id,
-    PARSE_TIMESTAMP('%m/%d/%Y %I:%M:%S %p', time) AS time,
+    PARSE_TIMESTAMP('%m/%d/%Y %I:%M:%S %p', time) AS time, --parsed the time field to convert to timestampt datatype 
     Value
 FROM `crack-case-431914-j8.Capstone.heart_rate_raw`
